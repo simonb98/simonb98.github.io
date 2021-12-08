b2283c3bab32d61efff51d74b2782491d63ac4ee
Simon Baeyens <43532756+simonb98@users.noreply.github.com>
2021-12-08 14:55:09 +0100

    skills redesign

---
+++ js/dist/scripts.dev.js
@@ -0,0 +1,128 @@
+"use strict";
+
+/*!
+ * Start Bootstrap - Creative v6.0.3 (https://startbootstrap.com/themes/creative)
+ * Copyright 2013-2020 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+ */
+(function ($) {
+  "use strict"; // Start of use strict
+  // Closes responsive menu when a scroll trigger link is clicked
+
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  }); // Activate scrollspy to add active class to navbar items on scroll
+
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 75
+  }); //fills backgroundcolor of navbar when scrolled down
+
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 100) {
+      $("#mainNav").addClass("navbar-scrolled");
+    } else {
+      $("#mainNav").removeClass("navbar-scrolled");
+    }
+  }); //moving pictures at top and contact page
+
+  var moveGallery = function moveGallery() {
+    var pictures = document.getElementsByClassName('picture');
+
+    for (var i = 0; i < pictures.length; i++) {
+      var x = Math.floor(Math.random() * 100);
+      var trans1 = "translate(" + x + "vw, 110vh)";
+      var trans2 = "translate(" + x + "vw, -110vh)";
+      pictures.item(i).animate([{
+        transform: trans1
+      }, {
+        transform: trans2
+      }], {
+        duration: Math.floor(Math.random() * 25000) + 10000,
+        iterations: Infinity,
+        delay: Math.floor(Math.random() * 20000) + 1500
+      });
+    }
+  };
+
+  moveGallery();
+  var tabList = document.querySelector(".tablist");
+  var tabs = document.querySelectorAll(".tablist__tab");
+  var activeTab = document.querySelector(".tablist__tab.is-active");
+  var selectionBar = document.querySelector(".selection-bar");
+
+  var toggleTabClass = function toggleTabClass(target, elements) {
+    Array.from(elements).forEach(function (element) {
+      return element.classList.remove("is-active");
+    });
+    target.classList.add("is-active");
+  };
+
+  var moveTabSelectionBar = function moveTabSelectionBar(targetTab) {
+    var rectTargetTab = targetTab.getBoundingClientRect();
+    var rectTabList = tabList.getBoundingClientRect();
+    var selectionBarWidth = rectTargetTab.width / rectTabList.width;
+    var selectionBarPosition = rectTargetTab.left - rectTabList.left;
+    selectionBar.style.transform = "matrix(".concat(selectionBarWidth, ", 0, 0, 1, ").concat(selectionBarPosition, ", 1)");
+  };
+
+  var showTabContent = function showTabContent(target) {
+    var story = document.getElementById("story");
+    var portfolio = document.getElementById("portfolio");
+
+    if (!target.className.toString().includes("is-active")) {
+      if (target.className.toString().includes("story")) {
+        setTimeout(function () {
+          document.getElementById("story").className = "";
+          document.getElementById("portfolio").className = "hidden";
+        }, 250);
+        story.animate([{
+          transform: "translate(150vw)"
+        }, {
+          transform: "translate(0vw)"
+        }], {
+          duration: 250,
+          delay: 250
+        });
+        portfolio.animate([{
+          transform: "translate(0vw)"
+        }, {
+          transform: "translate(-250vw)"
+        }], {
+          duration: 250
+        });
+      }
+
+      if (target.className.toString().includes("portfolio")) {
+        setTimeout(function () {
+          document.getElementById("story").className = "hidden";
+          document.getElementById("portfolio").className = "";
+        }, 250);
+        story.animate([{
+          transform: "translate(0vw)"
+        }, {
+          transform: "translate(150vw)"
+        }], {
+          duration: 250
+        });
+        portfolio.animate([{
+          transform: "translate(-250vw)"
+        }, {
+          transform: "translate(0vw)"
+        }], {
+          duration: 250,
+          delay: 250
+        });
+      }
+    }
+  };
+
+  Array.from(tabs).forEach(function (tab) {
+    tab.addEventListener("click", function (event) {
+      showTabContent(event.target);
+      toggleTabClass(event.target, tabs);
+      moveTabSelectionBar(event.target);
+    });
+  });
+  moveTabSelectionBar(activeTab);
+})(jQuery);
